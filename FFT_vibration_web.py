af2386e3ebb4eb11d08ec5f51769435d236ba8e7
HSIN YI HUNG <lavenderyi99@gmail.com>
2020-09-22 09:24:19 -0400

    add results for fft_test_replicate.py

--- FFT_vibration_web.py
+++ FFT_vibration_web.py
@@ -39,25 +39,3 @@ for x in range(data.shape[0]):
     for y in range(data.shape[1]):
         dataFFT[x,y,:] = scipy.fft(data[x,y,:])
 
-
-idxSS = np.random.randint(0,1023,size=(1000,2),dtype=np.int64)
-# Create a 1000x1000 similarity matrix...
-mtxDist = np.zeros((idxSS.shape[0] * (idxSS.shape[0]-1) //2,), dtype=np.float64)
-
-c = 0
-for i0 in tqdm(range(0, idxSS.shape[0])):
-    for i1 in range(i0+1, idxSS.shape[0]):
-        a = dataFFT[idxSS[i0,0], idxSS[i0,1], :]
-        b = dataFFT[idxSS[i1,0], idxSS[i1,1], :]
-        mtxDist[c] = np.linalg.norm(a - b)
-        c += 1
-        
-import fastcluster, time
-import scipy.spatial.distance, scipy.cluster.hierarchy
-import matplotlib.pyplot as plt
-
-t0 = time.time()
-clust = fastcluster.linkage(mtxDist, method='ward', preserve_input=True)
-t1 = time.time()
-t1 - t0
-        
